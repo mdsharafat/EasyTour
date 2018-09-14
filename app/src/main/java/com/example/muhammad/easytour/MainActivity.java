bb0e33a7a1a937375fdfd726ebf2c9f49e8f0cd5
Safkat Bin Jasib <40800154+SafkatTurza@users.noreply.github.com>
2018-09-15 02:44:24 +0600

    activity_main & main Java

--- app/src/main/java/com/example/muhammad/easytour/MainActivity.java
+++ app/src/main/java/com/example/muhammad/easytour/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.muhammad.easytour;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton mTravellerProfileIBtn,
+            mNearbyIBtn, mEventsIBtn,
+            mAllEventsIBtn, mHapppyMomentsIBtn,
+            mWeatherIBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTravellerProfileIBtn = findViewById(R.id.home_travelerImageIconBTN);
+        mNearbyIBtn = findViewById(R.id.home_nearbyIconBTN);
+        mEventsIBtn = findViewById(R.id.home_eventsIconIBtn);
+        mAllEventsIBtn = findViewById(R.id.home_allEventsIconIBtn);
+        mHapppyMomentsIBtn = findViewById(R.id.main_happyMoments_iconIBtn);
+        mWeatherIBtn = findViewById(R.id.main_weather_iconIBtn);
     }
 }
